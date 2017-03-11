fec6e2981129a3e344a99d6d9728470643548f36
CHUNIEE <21400337@handong.edu>
2017-03-11 16:47:32 +0900

    문제 03-1_5

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,16 +2,15 @@
 
 int main()
 {
-	int num1, num2 = 0;
-	int result1, result2 = 0;
+	int num1, num2, num3 = 0;
+	int result = 0;
 
-	printf("please input two integer : ");
-	scanf("%d %d", &num1, &num2);
+	printf("please input three integer : ");
+	scanf("%d %d %d", &num1, &num2, &num3);
 
-	result1 = num1 + num2;
-	result2 = num1 * num2;
+	result = (num1 - num2)*(num2 + num3)*(num3 % num1);
 
-	printf("sum : %d, verb : %d \n", result1, result2);
+	printf("result is %d \n", result);
 
 	return 0;
 }
