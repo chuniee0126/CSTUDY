a394c01154c0bebfac287a9b61b9b1af1cb9c121
CHUNIEE <21400337@handong.edu>
2017-03-13 22:26:17 +0900

    문제 08-2_1

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-	int index = 1;
-
-	printf("A program that outputs a multiple of 7 and a multiple of 9 from an integer greater than or equal to 1 and less than 100 ");
-
-	for (; index < 100; index++) {
-		
-		if (((index % 7) == 0) || ((index % 9) == 0))
-			printf("%d\n", index);
-
+	int index, index2 = 0;
+	
+	for (index = 1; index < 9; index++) {
+		if ((index % 2) == 1)
+			continue;
+		printf("\n%d´Ü Ãâ·Â\n", index);
+		for (index2 = 1; index2 <= index; index2++) {
+			printf("%d X %d = %d\n", index, index2, index*index2);
+		}
 	}
 
 	system("pause");
