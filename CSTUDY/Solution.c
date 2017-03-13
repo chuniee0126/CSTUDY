6f86004521323cde685a15a70e0c723a71ebdb37
CHUNIEE <21400337@handong.edu>
2017-03-14 00:37:01 +0900

    도전! 프로그래밍1_2

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,13 +3,26 @@
 
 int main(void)
 {
-	int num;
-	printf("input integer: ");
-	scanf("%d", &num);
+	int num1 = 0, num2 = 0, temp = 0;
+	printf("input two integer: ");
+	scanf("%d %d", &num1, &num2);
 
-	printf("%o, %d, %x", num, num, num);
+	if (num1 > num2) {
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
 
+	printf("\n");
+
+	for (; num1 <= num2; num1++) {
+		printf("%d단을 출력합니다.\n", num1);
+		for (int index = 1; index <= 9; index++) {
+			printf("%d X %d = %d\n", num1, index, num1*index);
+		}
+		printf("\n");
+	}
+	
 	system("pause");
-
 	return 0;
 }
