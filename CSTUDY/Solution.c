7fe33289935d4a0dcc5fbd71d81b513bccdebbd1
CHUNIEE <21400337@handong.edu>
2017-03-14 00:12:02 +0900

    도전! 프로그래밍1_1

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,35 +1,13 @@
 #include <stdio.h>
 
-int pib(int num) {
-	int pibo1 = 0, pibo2 = 1, index = 0;
-	int temp;
-
-	printf("피보나치수열 : \n");
-
-	for (; index < num; index++) {
-		if (index == 0)
-			printf("%d", pibo1);
-		else if (index == 1)
-			printf(", %d", pibo2);
-		else {
-			printf(", %d", pibo1 + pibo2);
-			temp = pibo1;
-			pibo1 = pibo2;
-			pibo2 = temp + pibo2;
-		}
-	}
-
-	printf("\n");
-		
-}
 
 int main(void)
 {
 	int num;
-	printf("자연수 입력: ");
+	printf("input integer: ");
 	scanf("%d", &num);
 
-	pib(num);
+	printf("%o, %d, %x", num, num, num);
 
 	system("pause");
 
