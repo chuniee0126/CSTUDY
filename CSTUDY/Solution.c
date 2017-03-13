1bbe54b707edd6db7d92563706e0bea1eba6f5c6
CHUNIEE <21400337@handong.edu>
2017-03-13 21:07:36 +0900

    문제 07-1_1

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,9 +2,17 @@
 
 int main(void)
 {
-	printf("%-8s %14s %5s \n", "이  름", "전공학과", "학년");
-	printf("%-8s %14s %5d \n", "김동수", "전자공학", 3);
-	printf("%-8s %14s %5d \n", "이을수", "컴퓨터공학", 2);
-	printf("%-8s %14s %5d \n", "한선영", "미술교육학", 4);
+	int num, index = 0;
+
+	printf("please input integer : ");
+	scanf("%d", &num);
+
+	while (index < num) {
+		printf("Hello world!\n");
+		index++;
+	}
+
+	system("pause");
+
 	return 0;
 }
