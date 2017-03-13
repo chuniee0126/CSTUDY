f4d63be408fec8d7b8e29b17188093fc16988fc1
CHUNIEE <21400337@handong.edu>
2017-03-13 22:37:49 +0900

    문제 08-2_2

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,14 +2,16 @@
 
 int main(void)
 {
-	int index, index2 = 0;
-	
-	for (index = 1; index < 9; index++) {
-		if ((index % 2) == 1)
-			continue;
-		printf("\n%d´Ü Ãâ·Â\n", index);
-		for (index2 = 1; index2 <= index; index2++) {
-			printf("%d X %d = %d\n", index, index2, index*index2);
+	int Anum, Znum = 0;
+	int result = 0;
+
+	printf("A program to obtain A and Z satisfying the following equation. \n AZ\n+ZA\n--------\n 99\n");
+
+	for (Anum = 0; Anum < 10; Anum++) {
+		for (Znum = 0; Znum < 10; Znum++) {
+			result = 10 * (Anum + Znum) + (Anum + Znum);
+			if (result == 99)
+				printf("A equal %d, Z equal %d \n", Anum, Znum);
 		}
 	}
 
