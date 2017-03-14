afff0c96f2414ae95e11eced22d083e87be917cb
CHUNIEE <21400337@handong.edu>
2017-03-14 21:47:44 +0900

    도전! 프로그래밍1_5

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,37 +3,23 @@
 
 int main(void)
 {
-	int money = 0, useful = 0, useful2 = 0, useful3 = 0;
-	int a = 0, b = 0, c = 0;
-	printf("현재 당신이 소유하고 있는 금액(원): ");
-	scanf("%d", &money);
-
-	useful = (money - 1600)/100;
-
-	for (a = 0; a <= (useful / 5); a++) {
-		useful2 = useful - a * 5;
-		if (useful2 > 0) {
-			for (b = 0; b <= (useful2 / 7); b++) {
-				useful3 = useful2 - b * 7;
-				if (useful3 > 0) {
-					for (c = 0; c <= (useful3 / 4); c++) {
-						if ((useful3 - c * 4) == 0) {
-							printf("크림빵 %d개, 새우깡 %d개, 콜 라 %d개\n", a + 1, b + 1, c + 1);
-						}
-					}
-				}
-				else if (useful3 == 0) {
-					printf("크림빵 %d개, 새우깡 %d개, 콜 라 %d개\n", a + 1, b + 1, c + 1);
-				}
-				c = 0;
+	int index = 0, count = 0, sum = 0, num = 2;
+	while (count < 10)
+	{
+		for (index = num - 1; index > 1; index--) {
+			if (num % index == 0) {
+				sum = 1;
 			}
 		}
-		else if (useful2 == 0) {
-			printf("크림빵 %d개, 새우깡 %d개, 콜 라 %d개\n", a + 1, b + 1, c + 1);
+
+		if (sum == 0) {
+			printf("%d ", num);
+			count++;
 		}
-		b = 0;
+
+		sum = 0;
+		num++;
 	}
-	printf("어떻게 구입하시겠습니까?");
 
 	system("pause");
 	return 0;
