38c9d9be2dd923f210f5e30a1fcc51a574018ac9
CHUNIEE <21400337@handong.edu>
2017-03-15 22:54:51 +0900

    문제 13-1_4

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,14 +3,18 @@
 
 int main(void)
 {
-	int arr[5] = { 1,2,3,4,5 };
-	int * ptr = arr;
-	
-	for (int index = 0; index < 5; index++) {
-		*(ptr + index) += 2;
+	int arr[6] = { 1,2,3,4,5,6 };
+	int * preptr = &arr[0];
+	int * postptr = &arr[5];
+	int temp;
+
+	for (int index = 0; index < 3; index++) {
+		temp = *(preptr + index);
+		*(preptr + index) = *(postptr - index);
+		*(postptr - index) = temp;
 	}
 
-	for (int index = 0; index < 5; index++) {
+	for (int index = 0; index < 6; index++) {
 		printf("%d \n", arr[index]);
 	}
 
