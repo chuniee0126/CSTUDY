7d343306c096316eaff9f67961dd8c545743aeee
CHUNIEE <21400337@handong.edu>
2017-03-15 22:22:06 +0900

    문제 12-1_2 포인터 문제

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-float function(int num) {
-	if (num == 0)
-		return 1;
-	else if (num > 0)
-		return 2 * function(num - 1);
-	else if (num < 0)
-		return 0.5 * function(num + 1);
-}
 
 int main(void)
 {
-	int num = 0;
-	
-	printf("정수 입력 : ");
-	scanf("%d", &num);
+	int num1 = 10, num2 = 20;
 
-	printf("2의 %d승은 %f\n", num, function(num));
+	int * ptr1 = &num1, * ptr2 = &num2;
+
+	*ptr1 = *ptr1 + 10;
+	*ptr2 = *ptr2 - 10;
+
+	ptr1 = &num2;
+	ptr2 = &num1;
+
+	printf("ptr1이 가리키는 변수 :%d, ptr2가 가리키는 변수 : %d\n", *ptr1, *ptr2);
 
 	system("pause");
 	return 0;
