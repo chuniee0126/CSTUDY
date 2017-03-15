0583a7b5d4e57259dce7b75b9fc1be90451b177e
CHUNIEE <21400337@handong.edu>
2017-03-15 23:14:46 +0900

    포인터 배열일 때 문자열을 저장하면 상수 형태의 문자열이 저장

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,21 +3,20 @@
 
 int main(void)
 {
-	int arr[6] = { 1,2,3,4,5,6 };
-	int * preptr = &arr[0];
-	int * postptr = &arr[5];
-	int temp;
+	char * strArr[3] = {"simple", "String", "Array"};
 
-	for (int index = 0; index < 3; index++) {
-		temp = *(preptr + index);
-		*(preptr + index) = *(postptr - index);
-		*(postptr - index) = temp;
-	}
+	printf("%s\n", strArr[0]);
+	printf("%s\n", strArr[1]);
+	printf("%s\n", strArr[2]);
 
-	for (int index = 0; index < 6; index++) {
-		printf("%d \n", arr[index]);
-	}
+	printf("%c\n", strArr[0][0]);
+
+	// strArr[0][0] = 'x'; 포인터 배열로 만들어진 상태에서 문자열을 저장하게 되었을 경우에는 상수 형태의 문자열이 저장되어 이 식에서 오류를 발생시킨다
+
+	printf("%c\n", strArr[0][0]);
 
 	system("pause");
+
 	return 0;
+	
 }
