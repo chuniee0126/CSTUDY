e885f81ebcd68c4c8e1d0bb69d9c68c6f80a6935
CHUNIEE <21400337@handong.edu>
2017-03-16 00:27:39 +0900

    도전! 프로그래밍2_3

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,17 +3,32 @@
 
 int main(void)
 {
-	char * strArr[3] = {"simple", "String", "Array"};
+	int arr[10];
+	int temp;
+	int countf = 0, countp = sizeof(arr) / sizeof(int) - 1;
 
-	printf("%s\n", strArr[0]);
-	printf("%s\n", strArr[1]);
-	printf("%s\n", strArr[2]);
+	printf("총 10개의 숫자 입력\n");
 
-	printf("%c\n", strArr[0][0]);
+	for (int index = 0; index < 10; index++) {
+		printf("입력 : ");
+		scanf("%d", &temp);
+		if (temp % 2 == 1) {
+			arr[countf] = temp;
+			countf++;
+		}
+		else {
+			arr[countp] = temp;
+			countp--;
+		}
+	}
 
-	// strArr[0][0] = 'x'; 포인터 배열로 만들어진 상태에서 문자열을 저장하게 되었을 경우에는 상수 형태의 문자열이 저장되어 이 식에서 오류를 발생시킨다
+	printf("배열 요소의 출력 : ");
 
-	printf("%c\n", strArr[0][0]);
+	for (int index = 0; index < 10; index++) {
+		printf("%d ", arr[index]);
+	}
+
+	printf("\n");
 
 	system("pause");
 
