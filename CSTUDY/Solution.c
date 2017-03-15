7d3468071fddeb15e532956dd440768f6c2bcbfb
CHUNIEE <21400337@handong.edu>
2017-03-15 22:46:58 +0900

    문제 13-1_1

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,17 +3,16 @@
 
 int main(void)
 {
-	int num1 = 10, num2 = 20;
+	int arr[5] = { 1,2,3,4,5 };
+	int * ptr = arr;
+	
+	for (int index = 0; index < 5; index++) {
+		*(ptr + index) += 2;
+	}
 
-	int * ptr1 = &num1, * ptr2 = &num2;
-
-	*ptr1 = *ptr1 + 10;
-	*ptr2 = *ptr2 - 10;
-
-	ptr1 = &num2;
-	ptr2 = &num1;
-
-	printf("ptr1이 가리키는 변수 :%d, ptr2가 가리키는 변수 : %d\n", *ptr1, *ptr2);
+	for (int index = 0; index < 5; index++) {
+		printf("%d \n", arr[index]);
+	}
 
 	system("pause");
 	return 0;
