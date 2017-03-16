5740b6f0ca28870d85021bcda8d31353f4ca834c
CHUNIEE <21400337@handong.edu>
2017-03-16 17:02:19 +0900

    도전! 프로그래밍2_5 버블sorting 오름차순 구현

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,25 +2,33 @@
 #include <string.h>
 
 
+void bubble_ascending(int * arr, int len) {
+	int temp;
+	for (int index = 0; index < (len - 1); index++) {
+		for (int index2 = 0; index2 < (len - 1 - index); index2++) {
+			if (arr[index2] > arr[index2 + 1]) {
+				temp = arr[index2];
+				arr[index2] = arr[index2 + 1];
+				arr[index2 + 1] = temp;
+			}
+		}
+	}
+}
+
+
 int main(void)
 {
-	char arr[100];
-	int count = 0;
+	int arr[5];
 
-	printf("문자열 입력 : ");
-
-	scanf("%s", arr);
-
-	for (unsigned int index = 0; index < (strlen(arr) / 2); index++) {
-		printf("%c, %c \n", arr[index], arr[strlen(arr) - 1 - index]);
-		if (arr[index] != arr[strlen(arr)- 1 - index])
-			count++;
+	for (int index = 0; index < 5; index++) {
+		scanf("%d", &arr[index]);
 	}
 
-	if (count == 0)
-		printf("회문 입니다. \n");
-	else
-		printf("회문이 아닙니다. \n");
+	bubble_ascending(arr, 5);
+
+	for (int index = 0; index < 5; index++) {
+		printf("%d ", arr[index]);
+	}
 
 	printf("\n");
 
