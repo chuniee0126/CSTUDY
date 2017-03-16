00e3266cf6078d38fd75c6a05624572dc556152a
CHUNIEE <21400337@handong.edu>
2017-03-16 15:50:18 +0900

    도전! 프로그래밍2_4

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(void)
 {
-	int arr[10];
-	int temp;
-	int countf = 0, countp = sizeof(arr) / sizeof(int) - 1;
+	char arr[100];
+	int count = 0;
 
-	printf("총 10개의 숫자 입력\n");
+	printf("문자열 입력 : ");
 
-	for (int index = 0; index < 10; index++) {
-		printf("입력 : ");
-		scanf("%d", &temp);
-		if (temp % 2 == 1) {
-			arr[countf] = temp;
-			countf++;
-		}
-		else {
-			arr[countp] = temp;
-			countp--;
-		}
+	scanf("%s", arr);
+
+	for (unsigned int index = 0; index < (strlen(arr) / 2); index++) {
+		printf("%c, %c \n", arr[index], arr[strlen(arr) - 1 - index]);
+		if (arr[index] != arr[strlen(arr)- 1 - index])
+			count++;
 	}
 
-	printf("배열 요소의 출력 : ");
-
-	for (int index = 0; index < 10; index++) {
-		printf("%d ", arr[index]);
-	}
+	if (count == 0)
+		printf("회문 입니다. \n");
+	else
+		printf("회문이 아닙니다. \n");
 
 	printf("\n");
 
