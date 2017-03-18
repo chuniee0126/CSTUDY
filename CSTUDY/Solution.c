183d32fd5ddbb0a6789e4128f24c4287bbb16879
CHUNIEE <21400337@handong.edu>
2017-03-19 02:24:08 +0900

    도전! 프로그래밍3_1 수정

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -4,27 +4,10 @@ void rotation(int (*arr)[4]) {
 	int i = 0;
 	int j = 0;
 	int temp[4][4];
-	//int * temp[4] = { &arr[0], &arr[1], &arr[2], &arr[3] };
-	/*for (int index = 0; index < 4; index++) {
-		for (int jndex = 0; jndex < 4; jndex++) {
-			temp[index][jndex] = arr[index][jndex];
-		}
-	}*/
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 4; j++) {
-			if (i == 0) {
-				temp[j][3] = (arr)[i][j];
-			}
-			if (i == 1) {
-				temp[j][2] = (arr)[i][j];
-			}
-			if (i == 2) {
-				temp[j][1] = (arr)[i][j];
-			}
-			if (i == 3) {
-				temp[j][0] = (arr)[i][j];
-			}
+			temp[j][3-i] = (arr)[i][j];
 		}
 	}
 
@@ -38,10 +21,9 @@ void rotation(int (*arr)[4]) {
 int main(void)
 {
 	int arr[4][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12}, {13,14,15,16} };
-	// int * arrts[4] = { arrt[0], arrt[1], arrt[2], arrt[3] };
 
 	for (int index = 0; index < 4; index++) {
-		rotation(arr);
+		
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				printf("%2d ", arr[i][j]);
@@ -49,6 +31,7 @@ int main(void)
 			printf("\n");
 		}
 		printf("\n");
+		rotation(arr);
 	}
 
 
