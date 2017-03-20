90fced4916a7adea00c35458a86a8a3fa9170f7d
CHUNIEE <21400337@handong.edu>
2017-03-20 19:17:31 +0900

    도전! 프로그래밍3_2 달팽이 배열 작성 프로그램

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,38 +1,54 @@
 #include <stdio.h>
 
-void rotation(int (*arr)[4]) {
-	int i = 0;
-	int j = 0;
-	int temp[4][4];
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			temp[j][3-i] = (arr)[i][j];
-		}
-	}
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			(arr)[i][j] = temp[i][j];
-		}
-	}
-}
-
 int main(void)
 {
-	int arr[4][4] = { {1,2,3,4} , {5,6,7,8} , {9,10,11,12}, {13,14,15,16} };
+	int arr[100][100];
+	int index = 0;
+	int interval = 0;
+	int num = 1;
+	int i = 0, j = 0;
 
-	for (int index = 0; index < 4; index++) {
-		
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				printf("%2d ", arr[i][j]);
+	printf("숫자를 입력하시오 : ");
+	scanf("%d", &index);
+
+	while (num < (index*index))
+	{
+		if (interval == 0) {
+			for (j = 0; j <= index-1; j++) {
+				arr[0][j] = num;
+				num++;
 			}
-			printf("\n");
+		}
+		else if (interval % 2 == 1) {
+			for (i = ((interval + 1) / 2); i <= (index - ((interval + 1) / 2)); i++) {
+				arr[i][j-1] = num;
+				num++;
+			}
+			for (j = (index - ((interval + 1) / 2) - 1); j >= (((interval + 1) / 2) - 1); j--) {
+				arr[i-1][j] = num;
+				num++;
+			}
+		}
+		else if ((interval) % 2 == 0) {
+			for (i = (index - (interval / 2) - 1); i >=  (interval / 2); i--) {
+				arr[i][j+1] = num;
+				num++;
+			}
+			for (j = (interval / 2); j <= (index - (interval / 2) - 1); j++) {
+				arr[i+1][j] = num;
+				num++;
+			}
+		}
+		interval++;
+	}
+
+	for (i = 0; i < index; i++) {
+		for (j = 0; j < index; j++) {
+			printf("%3d ", arr[i][j]);
 		}
 		printf("\n");
-		rotation(arr);
 	}
+
 
 
 	system("pause");
