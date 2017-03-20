a7e28d7519245b35f9f2c1e31f9c9077da653746
CHUNIEE <21400337@handong.edu>
2017-03-21 06:28:57 +0900

    도전! 프로그래밍3_6 야구게임 완성

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -2,81 +2,68 @@
 #include <stdlib.h>
 #include <time.h>
 
-void cheak(int temp, char temps[], char rock[], char scissor[], char paper[]) {
-	switch (temp)
-	{
-	case 1:
-		for (int i = 0; i < 15; i++) {
-			temps[i] = rock[i];
-		}
-		break;
-	case 2: 
-		for (int i = 0; i < 15; i++) {
-			temps[i] = scissor[i];
-		}
-		break;
-	case 3: 
-		for (int i = 0; i < 15; i++) {
-			temps[i] = paper[i];
-		}
-		break;
-	default:
-		break;
-	}
-}
-
-void results(int user, int computer, char result[]) {
-	char temp1[100] = "비겼습니다!";
-	char temp2[100] = "당신이 이겼습니다!";
-	char temp3[100] = "당신이 졌습니다!";
-	if (user == computer)
-		for (int i = 0; i < 100; i++) {
-			result[i] = temp1[i];
-		}
-	else if(((user - computer) == -1) || ((user - computer) == 2))
-		for (int i = 0; i < 100; i++) {
-			result[i] = temp2[i];
-		}
-	else
-		for (int i = 0; i < 100; i++) {
-			result[i] = temp3[i];
-		}
-}
-
-int main(void)
-{
-	char rock[15] = "바위";
-	char scissor[15] = "가위";
-	char paper[15] = "보";
-	char users[15];
-	char computers[15];
-	int user = 0;
-	int computer = 0;
-	char result[100];
+int random() {
 
 	srand((int)time(NULL));
 
-	while (1) {	
-		printf("바위는 1, 가위는 2, 보는 3: ");
-		scanf("%d", &user);
-		while (1) {
-			computer = rand();
-			if (computer >= 1 && computer <= 3)
-				break;
-		}
-		
-		cheak(user, users, rock, scissor, paper);
-		cheak(computer, computers, rock, scissor, paper);
-
-		results(user, computer, result);
-
-		printf("당신은 %s 선택, 컴퓨터는 %s 선택, %s \n", users, computers, result);
-
-		if (((user - computer) == 1) || ((user - computer) == -2))
+	int temp = 0;
+	
+	while (1) {
+		temp = rand();
+		if (temp >= 0 && temp <= 9)
 			break;
+	}
 
+	return temp;
+}
+
+
+int main(void)
+{
+	int cnum[3];
+	int unum[3];
+	int strike = 0, ball = 0, count = 1;
+
+	cnum[0] = random();
+	
+	while (1) {
+		cnum[1] = random();
+		if (cnum[1] != cnum[0])
+			break;
 	}
 	
+	while (1) {
+		cnum[2] = random();
+		if (cnum[2] != cnum[0] && cnum[2] != cnum[1])
+			break;
+	}
+
+	printf("Start Game!\n");
+
+	while (1) {
+		printf("3개의 숫자 선택 : ");
+		scanf("%d %d %d", &unum[0], &unum[1], &unum[2]);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (cnum[i] == unum[j]) {
+					if (i == j)
+						strike++;
+					else
+						ball++;
+					break;
+				}
+			}
+		}
+		printf("%d번째 도전 결과 : %dstrike, %dball!!\n", count, strike, ball);
+		
+		if (strike == 3)
+			break;
+		
+		strike = 0;
+		ball = 0;
+	}
+
+	printf("Game over!\n");
 
 	return 0;
 	
