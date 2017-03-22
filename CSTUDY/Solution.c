cde1104f0460a3c6a979302976a16b4c62431784
CHUNIEE <21400337@handong.edu>
2017-03-22 21:56:13 +0900

    문제 23-1 구조체 변수의 연산

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,49 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 
+typedef struct point {
+	int xpos;
+	int ypos;
+} Point;
 
-void emptyBuffer(void) {
-	while (getchar() != '\n');
+void Swap(Point * pnt1, Point * pnt2) {
+	
+	Point temp = *pnt1;
+	
+	*pnt1 = *pnt2;
+	*pnt2 = temp;
+
 }
 
-struct employee
-{
-	char name[10];
-	char perId[16];
-	long pee;
-} per[3];
-
 int main(void)
-{
-	int i;
-
-	for (i = 0; i < 3; i++) {
-
-		printf("%d번째 사람의 정보를 입력하세요.\n", i+1);
-
-		fputs("please input name : ", stdout);
-		fgets(per[i].name, sizeof(per[i].name), stdin);
-		per[i].name[strlen(per[i].name) - 1] = 0;
-
-		fputs("please input personal ID : ", stdout);
-		fgets(per[i].perId, sizeof(per[i].perId), stdin);
-		per[i].perId[strlen(per[i].perId) - 1] = 0;
-
-		fputs("please input pee : ", stdout);
-		scanf("%ld", &per[i].pee);
-		
-		puts("");
-
-		emptyBuffer();
-	}
-
+{	
+	Point pos1 = { 2,4 };
+	Point pos2 = { 5,7 };
 	
-	for (i = 0; i < 3; i++) {
-		printf("%d번 사원의 이름은 %s, 주민번호는 %s, 받고있는 급여는 %ld원 입니다.\n", i+1, per[i].name, per[i].perId, per[i].pee);
-	}
-	
+	printf("%d, %d /// %d, %d\n", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
 
+	Swap(&pos1, &pos2);
 
-	return 0;
-	
+	printf("%d, %d /// %d, %d\n", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+
+	return 0;	
 }
