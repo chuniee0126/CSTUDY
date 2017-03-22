c58a5a572ab0741c253fc2149e7801fa5e62c057
CHUNIEE <21400337@handong.edu>
2017-03-22 23:40:03 +0900

    파일 생성 및 쓰는 작업

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
-typedef struct point {
-	int xpos;
-	int ypos;
-} Point;
-
-void Swap(Point * pnt1, Point * pnt2) {
-	
-	Point temp = *pnt1;
-	
-	*pnt1 = *pnt2;
-	*pnt2 = temp;
-
-}
 
 int main(void)
 {	
-	Point pos1 = { 2,4 };
-	Point pos2 = { 5,7 };
-	
-	printf("%d, %d /// %d, %d\n", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+	FILE * fp = fopen("data.txt", "wt");
+	if (fp == NULL) {
+		puts("파일오픈 실패!");
+		return -1;
+	}
 
-	Swap(&pos1, &pos2);
-
-	printf("%d, %d /// %d, %d\n", pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+	fputc('A', fp);
+	fputc('B', fp);
+	fputc('C', fp);
+	fclose(fp);
 
 	return 0;	
 }
