b988d5ea999624e04831c71995101783f204760f
CHUNIEE <21400337@handong.edu>
2017-03-22 20:54:28 +0900

    문제 21-2_3 모범답안

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,46 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 
+int GetspaceIdx(char str[]) {
+	int len, i;
+	len = strlen(str);
+	for (i = 0; i < len; i++) {
+		if (str[i] == ' ')
+			return i;
+	}
+
+	return -1; // 공백 문자가 존재하지 않는 경우의 반환
+}
+
+
+int CompName(char str1[], char str2[]) {
+	int idx1 = GetspaceIdx(str1);
+	int idx2 = GetspaceIdx(str2);
+
+	if (idx1 != idx2)
+		return 0;
+	else
+		return !strncmp(str1, str2, idx1);
+}
+
+int CompAge(char str1[], char str2[]) {
+	int idx1 = GetspaceIdx(str1);
+	int idx2 = GetspaceIdx(str2);
+	int ag1, ag2;
+
+	ag1 = atoi(&str1[idx1 + 1]);
+	ag2 = atoi(&str2[idx1 + 1]);
+
+	if (ag1 == ag2)
+		return 1;
+	else
+		return 0;
+}
+
+
+
 int main(void)
 {
-	char arr[2][50];
-	int name[2][50];
-	int result[2] = { 0,0 };
-	int temp = 0;
-	int i, j, h;
+	char str1[20];
+	char str2[20];
 
-	for (i = 0; i < 2; i++) {
-		fgets(arr[i], sizeof(arr[i]), stdin);
-	}
+	printf("첫 번째 사람 정보 입력 : ");
+	fgets(str1, sizeof(str1), stdin);
+	str1[strlen(str1) - 1] = 0;
 
-	for (j = 0; j < 2; j++) {
-		for (h = 0; h < 50; h++) {
-			if (arr[j][h] == 32){
-				name[j][h] = NULL;
-				h++;
-				break;
-			}
-			name[j][h] = arr[j][h];
-		}
+	printf("두 번째 사람 정보 입력 : ");
+	fgets(str2, sizeof(str2), stdin);
+	str2[strlen(str2) - 1] = 0;
 
-		while (arr[j][h] != '\n') {
-			temp = arr[j][h] - 48;
-			result[j] = (result[j] * 10) + temp;
-			h++;
-		}
-	}
-
-	if (!strcmp(name[0], name[1]))
-		printf("이름이 같습니다. \n");
+	if (CompName(str1, str2))
+		puts("이름이 동일합니다!");
 	else
-		printf("이름이 같지 않습니다. \n");
+		puts("이름이 동일하지 않습니다!");
 
-	if (result[0] == result[1])
-		printf("나이가 같습니다. \n");
+	if (CompAge(str1, str2))
+		puts("나이가 같습니다!");
 	else
-		printf("나이가 같지 않습니다. \n");
-
-
+		puts("나이가 같지 않습니다!");
 
 	return 0;
 	
