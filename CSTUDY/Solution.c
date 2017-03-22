83537d60d5d3eb5ec568a4f2beda3479031dc8bb
CHUNIEE <21400337@handong.edu>
2017-03-22 23:45:29 +0900

    파일 읽기

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,15 +3,18 @@
 
 int main(void)
 {	
-	FILE * fp = fopen("data.txt", "wt");
+	int ch, i;
+	FILE * fp = fopen("data.txt", "rt");
 	if (fp == NULL) {
-		puts("파일오픈 실패!");
+		puts("파일오픈 실패");
 		return -1;
 	}
 
-	fputc('A', fp);
-	fputc('B', fp);
-	fputc('C', fp);
+	for (i = 0; i < 3; i++)
+	{
+		ch = fgetc(fp);
+		printf("%c \n", ch);
+	}
 	fclose(fp);
 
 	return 0;	
