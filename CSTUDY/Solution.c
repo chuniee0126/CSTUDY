32fe092fc3e0aacff16a82aea6c9519a6271f860
CHUNIEE <21400337@handong.edu>
2017-03-23 23:21:21 +0900

    바이너리 정보 복사 및 쓰는 작업

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -3,22 +3,37 @@
 
 int main(void)
 {
-	FILE * src = fopen("src.txt", "rt");
-	FILE * des = fopen("des.txt", "wt");
-	int str[20];
+	FILE * src = fopen("src.bin", "rb");
+	FILE * des = fopen("dst.bin", "wb");
+
+	char buf[20];
+	int readCnt;
 
 	if (src == NULL || des == NULL) {
 		puts("파일오픈 실패!");
 		return -1;
 	}
 
-	while (fgets(str, sizeof(str), src) != NULL)
-		fputs(str, des);
+	while (1)
+	{
+		readCnt = fread((void*)buf, 1, sizeof(buf), src);
 
-	if (feof(src) != 0)
-		puts("파일복사 완료!");
-	else
-		puts("파일복사 실패!");
+		if (readCnt < sizeof(buf))
+		{
+			if (feof(src) != 0)
+			{
+				fwrite((void*)buf, 1, readCnt, des);
+				puts("파일복사 완료");
+				break;
+			}
+			else
+				puts("파일복사 실패");
+		
+			break;
+		}
+		fwrite((void*)buf, 1, sizeof(buf), des); // 다시 나머지 정보를 받아와서 복사를 진행하기 위해 처음으로 돌아간다.
+
+	}
 
 	fclose(src);
 	fclose(des);
