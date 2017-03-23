bd0b96840492ae01c419d32cbb4a50de3067e46c
CHUNIEE <21400337@handong.edu>
2017-03-23 22:45:06 +0900

    파일 복사(문자열 단위)

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -5,15 +5,15 @@ int main(void)
 {
 	FILE * src = fopen("src.txt", "rt");
 	FILE * des = fopen("des.txt", "wt");
-	int ch;
+	int str[20];
 
 	if (src == NULL || des == NULL) {
 		puts("파일오픈 실패!");
 		return -1;
 	}
 
-	while ((ch = fgetc(src)) != EOF)
-		fputc(ch, des);
+	while (fgets(str, sizeof(str), src) != NULL)
+		fputs(str, des);
 
 	if (feof(src) != 0)
 		puts("파일복사 완료!");
