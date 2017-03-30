803449a7880afc297a2c5468d1cb4440c6cf2f07
CHUNIEE <21400337@handong.edu>
2017-03-30 22:29:37 +0900

    <실습2> - 1시간 (경진) 포인터 2중배열 위치 바꾸기 (15분)

--- CSTUDY/Solution.c
+++ CSTUDY/Solution.c
@@ -1,119 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 
-int check(int * arrt) {
-
+void Swap(int (*arr)[3])
+{
 	int temp = 0;
+	int i = 0, j = 0;
 
-	while (1) {
-		scanf("%d", &temp);
-
-		if (arrt[temp - 1] == 0 && temp > 0 && temp <= 16) {
-			arrt[temp - 1] = 1;
-			return temp;
-		}
-		else
-			printf("please another input : ");
-	}
-}
-
-int BINGO(int rdn, int (*arr)[4], int (*bingo)[4], int (*bingocheck)[2]) {
-	
-	int i = 0, j = 0, h = 0;
-	int num = 0;
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			if (arr[i][j] == rdn) {
-				h = 1;
-				break;
-			}
-		}
-		if (h == 1)
-			break;
+	for (j = 0; j < 3; j++) {
+		temp = arr[2][j];
+		arr[2][j] = arr[1][j];
+		arr[1][j] = arr[0][j];
+		arr[0][j] = temp;
 	}
 
-	bingo[i][j] = arr[i][j];
-
-	bingocheck[i][0]++;
-	bingocheck[j][1]++;
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 2; j++) {
-			if (bingocheck[i][j] >= 4)
-				num++;
-		}
-	}
-
-	return num;
-
+	return;
 }
 
 
 int main(void)
 {
-	int arr[4][4];
+	int arr[3][3];
 	int i = 0, j = 0;
-	int temp = 0, rdn = 0;
-	int checkint[16];
-	int checkint2[16];
-	int bingo[4][4];
-	int bingocheck[4][2];
 
-	srand((int)time(NULL));
 
-	for (i = 0; i < 16; i++) {
-		checkint[i] = 0;
-		checkint2[i] = 0;
-	}
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 2; j++) {
-			bingocheck[i][j] = 0;
+	for (i = 0; i < 3; i++) {
+		printf("%d번 라인 채우기 : ", i + 1);
+		for (j = 0; j < 3; j++) {
+			scanf("%d", &arr[i][j]);
 		}
 	}
 
-	for (i = 0; i < 4; i++) {
-		printf("please input %d line : ", i+1);
-		for (j = 0; j < 4; j++) {
-			temp = check(checkint);
-			arr[i][j] = temp;
-			bingo[i][j] = 0;
-		}
-	}
+	printf("초기 어레이\n");
 
-	printf("만들어진 빙고판\n");
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			printf("%3d ", arr[i][j]);
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", arr[i][j]);
 		}
 		printf("\n");
 	}
 
+	Swap(arr);
 
-	while (1) {
-		rdn = rand() % 16;
-		if (rdn < 0 && rdn > 16 || checkint2[rdn] != 0)
-			continue;
-		else if(checkint2[rdn] == 0)
-		{
-			checkint2[rdn]++;
-			if (BINGO(rdn + 1, arr, bingo, bingocheck) >= 3)
-				break;
-		}
-	}
+	printf("수정한 어레이\n");
 
-	printf("빙고완료\n");
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			printf("%3d ", bingo[i][j]);
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", arr[i][j]);
 		}
 		printf("\n");
 	}
-	
-	
+
 
 	return 0;
 }
